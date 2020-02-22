84aecfd0b671d5d765ef906693e40a99ab801c6d
pauleanderson <pauleanderson@gmail.com>
2020-02-22 16:41:48 +0000

    update

--- dev_server/start_dev.sh
+++ dev_server/start_dev.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+export PORT=$1
+echo "Starting on PORT:"$PORT
 source /data/env/bin/activate
 
 export GUROBI_HOME="/usr/local/gurobi811/linux64"
 export PATH="${PATH}:${GUROBI_HOME}/bin" 
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib" 
 
-nohup jupyter lab  --ip=0.0.0.0 --port=9999 --LabApp.base_url=jupyter --notebook-dir / >/tmp/jupyter.log 2>&1 &
+nohup jupyter lab  --ip=0.0.0.0 --port=$PORT --LabApp.base_url=jupyter --notebook-dir /disk >/tmp/jupyter_$PORT.log 2>&1 &
